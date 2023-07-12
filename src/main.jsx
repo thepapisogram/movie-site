156d568be2eb94f4cde980f99a96a6e536f5af4f
Anthony Saah <thepapisogram@gmail.com>
2023-07-12 10:56:29 +0000

    Route Error Fixed: now adding client side routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,48 +1,29 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-import App from './routes/app';
-import Home from './routes/home/home';
-import Search from './routes/search';
+import App from "./routes/app";
+import Home from "./routes/home/home";
+import Search from "./routes/search";
 
-import Movie from './routes/movies/movie';
-import Movies from './routes/movies/movies';
+import Movie from "./routes/movies/movie";
+import Movies from "./routes/movies/movies";
 
-import About from './routes/about';
+import About from "./routes/about";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    errorElement: <h1>Failed to load page</h1>,
-    children: [
-      {
-        path: "/",
-        element: <Home />,
-      },
-      {
-        path: "/search",
-        element: <Search />,
-      },
-      {
-        path: "/movies",
-        element: <Movies />
-      },
-      {
-        path: "/movies/:id",
-        element: <Movie />
-      },
-      {
-        path: "/about",
-        element: <About />,
-      },
-    ],
-  },
-]);
-
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
+    <BrowserRouter>
+      <App>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/search" element={<Search />} />
+          <Route path="/movies" element={<Movies />} />
+          <Route path="/movies/:id" element={<Movie />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </App>
+    </BrowserRouter>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
